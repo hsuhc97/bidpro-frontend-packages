49257fc5f5a9dfe2d450a17cd513af6de5d59a34
hhhsu <hcxu0814@gmail.com>
2025-04-30 16:14:35 +0800

    auction type

--- shared/src/api/services/customer/auction/types.ts
+++ shared/src/api/services/customer/auction/types.ts
@@ -1,5 +1,12 @@
 export type Auction = {
     id: string;
+    title: string;
+    subTitle: string;
+    attributes: Record<string, string>;
+    quantity: number;
+    actualPrice: number;
+    askingMinOfferPrice: number;
+    askingMaxOfferPrice: number;
     closeTime: number;
     type: number;
     status: number;
